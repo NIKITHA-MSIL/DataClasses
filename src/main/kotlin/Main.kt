2c85407fc6b9339f1d75ddc14f02b3f609c7a1d5
91949 <NIKITHA>
2022-06-12 18:57:18 +0530

    DataClasses in kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    data class man(val name: String)
+    {
+
+        var height: Int = 0;
+    }
+
+
+
+    val man1=man("manish")
+
+    man1.height = 70
+
+
+    println(man1.toString());
+
+
+    println(man1.height);
+}
